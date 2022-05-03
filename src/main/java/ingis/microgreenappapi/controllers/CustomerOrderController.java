dea0da9044dcc6529098e489e427d596d651eb20
Ingrid Barrett <barrett92@yahoo.com>
2022-05-03 13:05:24 -0400

    Added CustomerOrderController and pseudocode

---
+++ src/main/java/ingis/microgreenappapi/controllers/CustomerOrderController.java
@@ -0,0 +1,23 @@
+package ingis.microgreenappapi.controllers;
+
+public class CustomerOrderController {
+
+    //add new order
+        //enter Customer name
+        // enter seed, tray size, qty
+            //calculate seed order for tray size
+                //if not enough seed, send error message
+            //Are there enough trays item
+                // if not, send error message
+             // is there enough panting medium for tray size
+                // if not, send error message
+        //reduce inventory(seed, tray, planting medium)
+
+
+    //order cancelled before planting
+        // new order
+
+    //order delivered
+        //add trays back into inventory
+
+}
