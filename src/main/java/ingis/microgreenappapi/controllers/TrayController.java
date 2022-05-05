57f1828725a04d37fd3d8aaaf8dd02c544cb40e9
Ingrid Barrett <barrett92@yahoo.com>
2022-05-05 12:37:50 -0400

    Add tray method working.  Lies at path /trays/add

--- src/main/java/ingis/microgreenappapi/controllers/TrayController.java
+++ src/main/java/ingis/microgreenappapi/controllers/TrayController.java
@@ -5,12 +5,12 @@ import ingis.microgreenappapi.data.TrayRepository;
 import ingis.microgreenappapi.models.Seed;
 import ingis.microgreenappapi.models.Tray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/trays")
 public class TrayController {
 
     @Autowired
@@ -18,9 +18,38 @@ public class TrayController {
 
     // view all tray information
 
-    @GetMapping("/trays")
+    @GetMapping
     public List<Tray> getTrays() {
         return trayRepo.findAll();
     }
+
+    @PostMapping(value = "/add")
+    public String addTrays(@RequestBody Tray tray) {
+        trayRepo.save(tray);
+        return "Saved....";
+    }
+
+//    @PutMapping(value = "/update/{seedId}")
+//    public String updateSeed(@PathVariable(value = "seedId") Integer seedId, @RequestBody Seed seed) {
+//        Seed updatedSeed = seedRepo.findById(seedId).get();
+//        updatedSeed.setSeedName(seed.getSeedName());
+//        updatedSeed.setSeedingDensity(seed.getSeedingDensity());
+//        updatedSeed.setSeedPresoak(seed.getSeedPresoak());
+//        updatedSeed.setBlackoutTime(seed.getBlackoutTime());
+//        updatedSeed.setQty(seed.getQty());
+//
+//        seedRepo.save(updatedSeed);
+//        return "updated....";
+//    }
+//
+//    @DeleteMapping(value = "/delete/{seedId}")
+//    public String deleteSeed(@PathVariable Integer seedId) {
+//        Seed deletedSeed = seedRepo.findById(seedId).get();
+//        seedRepo.delete(deletedSeed);
+//        return "deleted...";
+//    }
+
+
+
 
 }
