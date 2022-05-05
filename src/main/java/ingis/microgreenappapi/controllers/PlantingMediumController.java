db17f4773b76db93fea9aace76aef54db4021ee9
Ingrid Barrett <barrett92@yahoo.com>
2022-05-05 13:59:50 -0400

    Add plantingMedium method working.  Lies at path /medium/add.  Delete plantingMedium method working.  Lies at path /delete/add.  Did some refactoring, remove unneeded imports.

--- src/main/java/ingis/microgreenappapi/controllers/PlantingMediumController.java
+++ src/main/java/ingis/microgreenappapi/controllers/PlantingMediumController.java
@@ -1,10 +1,7 @@
 package ingis.microgreenappapi.controllers;
 
 import ingis.microgreenappapi.data.PlantingMediumRepository;
-import ingis.microgreenappapi.data.SeedRepository;
 import ingis.microgreenappapi.models.PlantingMedium;
-import ingis.microgreenappapi.models.Seed;
-import ingis.microgreenappapi.models.Tray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,34 +14,37 @@ public class PlantingMediumController {
     @Autowired
     private PlantingMediumRepository mediumRepo;
 
-    // view all planting medium information
-
+// view all planting medium information
     @GetMapping
     public List<PlantingMedium> getMedium() {
         return mediumRepo.findAll();
-    } @PostMapping(value = "/add")
-    public String addTrays(@RequestBody PlantingMedium medium) {
+    }
+
+// add planting medium
+    @PostMapping(value = "/add")
+    public String addMedium(@RequestBody PlantingMedium medium) {
         mediumRepo.save(medium);
         return "Saved....";
     }
 
-//    // add planting medium
-//    @PutMapping(value = "/update/{mediumId}")
-//    public String updateMedium(@PathVariable(value = "mediumId") Integer mediumId, @RequestBody PlantingMedium medium) {
-//        PlantingMedium updatedMedium = mediumRepo.findById(mediumId).get();
-//        updatedMedium.setMediumType(medium.getMediumType());
-//        updatedMedium.setQty(medium.getQty());
-//        mediumRepo.save(updatedMedium);
-//        return "updated....";
-//    }
-//
+// update planting medium
+    @PutMapping(value = "/update/{mediumId}")
+    public String updateMedium(@PathVariable(value = "mediumId") Integer mediumId,
+                               @RequestBody PlantingMedium medium) {
+        PlantingMedium updatedMedium = mediumRepo.findById(mediumId).get();
+        updatedMedium.setMediumType(medium.getMediumType());
+        updatedMedium.setQty(medium.getQty());
+        mediumRepo.save(updatedMedium);
+        return "updated....";
+    }
+
+// delete planting medium
     @DeleteMapping(value = "/delete/{mediumId}")
     public String deleteMedium(@PathVariable Integer mediumId) {
         PlantingMedium deletedMedium = mediumRepo.findById(mediumId).get();
         mediumRepo.delete(deletedMedium);
         return "deleted...";
     }
-
 
 }
 
