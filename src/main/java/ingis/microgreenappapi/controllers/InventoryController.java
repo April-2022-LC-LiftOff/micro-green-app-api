63464b19282d87d820f661a70f21564c902a427b
Ingrid Barrett <barrett92@yahoo.com>
2022-05-09 14:36:23 -0400

    InventoryController - added view seed, add seed, update seed & delete seed to /inventory

--- src/main/java/ingis/microgreenappapi/controllers/InventoryController.java
+++ src/main/java/ingis/microgreenappapi/controllers/InventoryController.java
@@ -5,12 +5,11 @@ import ingis.microgreenappapi.models.Seed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 //
 @RestController
+@RequestMapping("/inventory")
 //@RequestMapping
 public class InventoryController {
 
@@ -19,7 +18,7 @@ public class InventoryController {
 
     // view all seed information
 
-    @GetMapping("/inventory")
+    @GetMapping("/view")
     public List<Seed> viewInventory() {
         return seedRepo.findAll();
     }
@@ -30,56 +29,22 @@ public class InventoryController {
         return "Saved....";
     }
 
-//    public List<Seed> getSeeds() {
-//        return seedRepo.findAll();
-//    }
+    @PutMapping(value = "/update/{seedId}")
+    public String updateSeed(@PathVariable(value = "seedId") Integer seedId, @RequestBody Seed seed) {
+        Seed updatedSeed = seedRepo.findById(seedId).get();
+        updatedSeed.setSeedName(seed.getSeedName());
+        updatedSeed.setQty(seed.getQty());
 
-//    @GetMapping("/inventory")
-//    public ArrayList<Object> getInventory() {
-//    public String getInventory() {
-//
-//        ArrayList<Object> currentInventory = new ArrayList<>();
-////        // Adding elements to the Map
-////        // using standard put() method
-////        Seed updatedSeed = seedRepo.findById(1).get();
-////        updatedSeed.setSeedName(seed.getSeedName());
-//
-//
-////        System.out.println(name);
-//        String name = seedRepo.findById(15).get().getSeedName();
-//        Integer qty = seedRepo.findById(15).get().getQty();
-//        Object obj = name,
-//        currentInventory.put(name,qty,);
-//        currentInventory.put("8x6 base", 30);
-//        currentInventory.put("promix", 20);
-////
-//        return currentInventory;
-//        return "found";
+        seedRepo.save(updatedSeed);
+        return "updated....";
+    }
 
-
-
-//
-
-//        return map;
-//    }
-//   HashMap<String, Integer> currentInventory = new HashMap<>();
-
-//        HashMap<String, Integer> map = new HashMap<>();
-//
-//        // Adding elements to the Map
-//        // using standard put() method
-//        map.put("broccoli", 10);
-//        map.put("8x6 base", 30);
-//        map.put("promix", 20);
-//
-//        // Print size and content of the Map
-//        System.out.println("Size of map is:- "
-//                + map.size());
-//
-//        // Printing elements in object of Map
-//        System.out.println(map);
-
-
+    @DeleteMapping(value = "/delete/{seedId}")
+    public String deleteSeed(@PathVariable Integer seedId) {
+        Seed deletedSeed = seedRepo.findById(seedId).get();
+        seedRepo.delete(deletedSeed);
+        return "deleted...";
+    }
 
 
 }
