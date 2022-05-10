c4259a94a04866e5417db98ebbf777c6f25f2903
Sharon Konki <sharonkonki@gmail.com>
2022-05-10 09:15:33 -0500

    "added basic crud functionality to customer orders along with mapping paths for each action"

--- src/main/java/ingis/microgreenappapi/controllers/CustomerOrderController.java
+++ src/main/java/ingis/microgreenappapi/controllers/CustomerOrderController.java
@@ -1,8 +1,66 @@
 package ingis.microgreenappapi.controllers;
 
+import ingis.microgreenappapi.models.CustomerOrder;
+import ingis.microgreenappapi.repositories.CustomerOrderRepo;
+import ingis.microgreenappapi.repositories.OrderDetailsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
 public class CustomerOrderController {
 
-    //add new order
+
+    @Autowired
+    private CustomerOrderRepo customerOrderRepo;
+
+
+    //get all orders
+    @GetMapping("/view")
+    public List<CustomerOrder> getAllCustomerOrders(){
+        List<CustomerOrder> customerOrders = new ArrayList<>();
+        customerOrderRepo.findAll().forEach(customerOrders::add);
+        return customerOrders;
+    }
+
+    //get order by id
+    @GetMapping("/get/{orderId}")
+    public CustomerOrder getCustomerOrder(int orderId){
+        CustomerOrder customerOrder = customerOrderRepo.findById(orderId).orElse(null);
+        return customerOrder;
+    }
+
+    //add new customer order
+    @PostMapping("/add")
+    public CustomerOrder addCustomerOrder(CustomerOrder customerOrder){
+        customerOrderRepo.save(customerOrder);
+        return customerOrder;
+    }
+
+    //update customer order
+    @PutMapping("/update/{orderId}")
+    public CustomerOrder updateCustomerOrder(int orderId){
+        CustomerOrder customerOrder = customerOrderRepo.findById(orderId).orElse(null);
+        customerOrderRepo.save(customerOrder);
+        return customerOrder;
+    }
+
+    //delete order by id
+    @DeleteMapping("/delete/{orderId}")
+    public CustomerOrder deleteCustomerOrder(int orderId){
+        CustomerOrder customerOrder = customerOrderRepo.findById(orderId).orElse(null);
+        customerOrderRepo.deleteById(orderId);
+        return customerOrder;
+    }
+
+
+}
+
+
+        //add new order
         //enter Customer name
         // enter seed, tray size, qty
             //calculate seed order for tray size
@@ -20,4 +78,3 @@ public class CustomerOrderController {
     //order delivered
         //add trays back into inventory
 
-}
--- src/main/java/ingis/microgreenappapi/models/CustomerOrder.java
+++ src/main/java/ingis/microgreenappapi/models/CustomerOrder.java
@@ -1,91 +1,106 @@
 package ingis.microgreenappapi.models;
 
+import javax.persistence.*;
 import java.lang.reflect.Type;
 import java.util.Date;
 import java.util.Objects;
 
-//@Entity
+@Entity
+@Table(name = "customer_orders")
 public class CustomerOrder {
 
-//    @Id
-//    @GeneratedValue
+    @Id
+    @GeneratedValue
 //    @OneToMany
-//    private int orderId;
-//    private static int nextId = 1;
-//
-//    private Type customerId;
-//    private Date orderDate;
-//    private Date deliveryDate;
-//    private Boolean activeOrder;
-//
-//    @OneToMany
-//    private OrderDetails OrderDetails[];
-//
-//    public CustomerOrder(Type customerId, Date orderDate, Date deliveryDate, Boolean activeOrder, ingis.microgreenappapi.models.OrderDetails[] orderDetails) {
-//        this.customerId = customerId;
-//        this.orderDate = orderDate;
-//        this.deliveryDate = deliveryDate;
-//        this.activeOrder = activeOrder;
-//        OrderDetails = orderDetails;
-//        this.orderId = nextId;
-//        nextId++;
-//
-//    }
-//
-//    public int getOrderId() {
-//        return orderId;
-//    }
-//
-//    public Type getCustomerId() {
-//        return customerId;
-//    }
-//
-//    public void setCustomerId(Type customerId) {
-//        this.customerId = customerId;
-//    }
-//
-//    public Date getOrderDate() {
-//        return orderDate;
-//    }
-//
-//    public void setOrderDate(Date orderDate) {
-//        this.orderDate = orderDate;
-//    }
-//
-//    public Date getDeliveryDate() {
-//        return deliveryDate;
-//    }
-//
-//    public void setDeliveryDate(Date deliveryDate) {
-//        this.deliveryDate = deliveryDate;
-//    }
-//
-//    public ingis.microgreenappapi.models.OrderDetails[] getOrderDetails() {
-//        return OrderDetails;
-//    }
-//
-//    public void setOrderDetails(ingis.microgreenappapi.models.OrderDetails[] orderDetails) {
-//        OrderDetails = orderDetails;
-//    }
-//
-//    public Boolean getActiveOrder() {
-//        return activeOrder;
-//    }
-//
-//    public void setActiveOrder(Boolean activeOrder) {
-//        this.activeOrder = activeOrder;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        CustomerOrder that = (CustomerOrder) o;
-//        return orderId == that.orderId;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(orderId);
-//    }
+    private int orderId;
+    private static int nextId = 1;
+
+    private Date orderDate;
+    private Date deliveryDate;
+    private Boolean activeOrder;
+
+    private  Customer customer;
+    @OneToMany
+    private OrderDetails orderDetails;
+
+    public CustomerOrder(int orderId, Date orderDate, Date deliveryDate, Boolean activeOrder, Customer customer, OrderDetails orderDetails) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.activeOrder = activeOrder;
+        this.customer = customer;
+        this.orderDetails = orderDetails;
+    }
+
+    public  CustomerOrder(){
+
+    }
+
+    @Column(name = "order_id")
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    @Column(name = "order_date")
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Column(name = "delivery_date")
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Column(name = "active_order")
+    public Boolean getActiveOrder() {
+        return activeOrder;
+    }
+
+    public void setActiveOrder(Boolean activeOrder) {
+        this.activeOrder = activeOrder;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false, foreignKey = @ForeignKey(name = "customer_order_customer_FK"))
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "order_details", nullable = false, foreignKey = @ForeignKey(name = "customer_order_orderdetail_FK"))
+    public OrderDetails getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(OrderDetails orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerOrder{" + "orderId =" + orderId +
+                ", customer = " + customer +
+                ", orderDate = " + orderDate +
+                ", deliveryDate =" + deliveryDate +
+                ", activeOrder = " + activeOrder +
+                ", orderDetails = " + orderDetails +
+                '}';
+    }
+
 }
