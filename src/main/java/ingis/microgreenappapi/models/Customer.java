3904e6b1844a36066ba1fd03fd88bf862412914f
Brian Williams <Bkwilliamscb@gmail.com>
2022-05-11 19:27:54 -0500

    Added Customer Model, Controller, Repository, and Data.

--- src/main/java/ingis/microgreenappapi/models/Customer.java
+++ src/main/java/ingis/microgreenappapi/models/Customer.java
@@ -1,48 +1,62 @@
 package ingis.microgreenappapi.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-public class Customer {
+@Entity
+public class Customer{
 
 
-//    @OneToMany
-//    private int customerId;
-//    private static int nextId = 1;
-//
-//    @Size (max = 50, message = "Name too long!")
-//    private String customerName;
-//
-//    public Customer(String customerName) {
-//        this.customerName = customerName;
-//        this.customerId = nextId;
-//        nextId++;
-//    }
-//
-//    public String getCustomerName() {
-//        return customerName;
-//    }
-//
-//    public void setCustomerName(String customerName) {
-//        this.customerName = customerName;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Customer{" +
-//                "customerName='" + customerName + '\'' +
-//                '}';
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Customer customer = (Customer) o;
-//        return customerId == customer.customerId;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(customerId);
-//    }
+    //    @OneToMany
+    @Id
+    @GeneratedValue
+    private int customerId;
+    private static int nextId = 1;
+
+    @Size(max = 50, message = "Name too long!")
+    private String customerName;
+
+    public Customer() {}
+
+    public Customer(String customerName) {
+        this.customerName = customerName;
+        this.customerId = nextId;
+        nextId++;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+
+    public int getId() {
+        return customerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerName='" + customerName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customerId == customer.customerId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId);
+    }
 }
