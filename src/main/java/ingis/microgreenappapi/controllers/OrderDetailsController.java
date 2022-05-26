32831088e0a7561bb3a5aa0860fd5af44d43f046
Ingrid Barrett <barrett92@yahoo.com>
2022-05-26 18:46:11 -0400

    Added CrossOrigin annotation added to Seed detail.  Added seeding density to seedQty calculation. Added deliveryDate variable to addOrderDetail.

--- src/main/java/ingis/microgreenappapi/controllers/OrderDetailsController.java
+++ src/main/java/ingis/microgreenappapi/controllers/OrderDetailsController.java
@@ -45,10 +45,84 @@ public class OrderDetailsController {
             return "Not enough inventory for order";
         }
         // adjust seed inventory
-        seed.setQty(seed.getQty() - orderDetail.getQty());
+        seed.setQty(seed.getQty() - (orderDetail.getQty() * seed.getSeedingDensity()));
 
         //Create tasks
         LocalDate dd = LocalDate.parse("2022-05-30");
+
+        String todayTask;
+        Task task;
+
+        if (seed.getSeedPresoak()) {
+            //Create soak tasks
+            todayTask = "Soak " + seed.getSeedName();
+            task = new Task();
+            task.setTask(todayTask);
+            task.setDueDate(dd.minusDays(seed.getHarvestTime()+1));
+            taskRepo.save(task);
+        }
+
+        //Create water above days
+        for (int i = 1; i < seed.getBlackoutTime(); i++) {
+            if (i == 1) {
+                //Plant & water from above
+                todayTask = "Plant " + seed.getSeedName() + ", cover & move to dark racks";
+                task = new Task();
+                task.setTask(todayTask);
+                task.setDueDate(dd.minusDays(seed.getHarvestTime()));
+                taskRepo.save(task);
+            }
+            //Just water from above
+            todayTask = "Water " + seed.getSeedName() + " from above";
+            task = new Task();
+            task.setTask(todayTask);
+            task.setDueDate(dd.minusDays(seed.getHarvestTime()-i));
+            taskRepo.save(task);
+        }
+
+        // Create water below days
+        for (int i = (seed.getHarvestTime()- seed.getBlackoutTime()); i > 0; i--) {
+            if (i == (seed.getHarvestTime()- seed.getBlackoutTime())) {
+                todayTask = "Move " + seed.getSeedName() + " lighted racks and water below";
+                task = new Task();
+                task.setTask(todayTask);
+                task.setDueDate(dd.minusDays(i));
+                taskRepo.save(task);
+            } else {
+                todayTask = "Water " + seed.getSeedName() + " from below";
+                task = new Task();
+                task.setTask(todayTask);
+                task.setDueDate((dd.minusDays(i)));
+                taskRepo.save(task);
+            }
+        }
+
+        //Create pull for delivery
+        todayTask = "Pull " + seed.getSeedName() + " for delivery";
+        task = new Task();
+        task.setTask(todayTask);
+        task.setDueDate(dd);
+        taskRepo.save(task);
+
+        seedRepo.save(seed);
+        detailRepo.save(orderDetail);
+//        return newQty.toString();
+        return " processed";
+    }
+
+    @PostMapping(value = "/add/{deliveryDate}")
+    public String addOrderDetail(@PathVariable String deliveryDate, @RequestBody OrderDetails orderDetail) {
+
+        Seed seed = seedRepo.findById(orderDetail.getSeedRefId()).get();
+        // Check to see if enough inventory
+        if (seed.getQty() - orderDetail.getQty() < 0) {
+            return "Not enough inventory for order";
+        }
+        // adjust seed inventory
+        seed.setQty(seed.getQty() - (orderDetail.getQty() * seed.getSeedingDensity()));
+
+        //Create tasks
+        LocalDate dd = LocalDate.parse(deliveryDate);
 
         String todayTask;
         Task task;
