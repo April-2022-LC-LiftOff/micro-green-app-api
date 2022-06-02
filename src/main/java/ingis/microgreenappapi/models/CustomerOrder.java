be10b26e6971feb15a9ce17126ff0a818b2a1d66
Ingrid Barrett <barrett92@yahoo.com>
2022-06-02 16:04:09 -0400

    duplicate customer added corrected

--- src/main/java/ingis/microgreenappapi/models/CustomerOrder.java
+++ src/main/java/ingis/microgreenappapi/models/CustomerOrder.java
@@ -17,21 +17,23 @@ import java.util.*;
 @Table(name = "customer_orders")
 public class CustomerOrder {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int orderId;
-    private static int nextId = 1;
 
     @Column(name = "order_date")
     private LocalDate orderDate;
+
     @Column(name = "delivery_date")
     private LocalDate deliveryDate;
+
     @Column(name = "active_order")
     private Boolean activeOrder;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     @JoinColumn(name="orderId")
     private  List<OrderDetails> orderDetails = new ArrayList<>();
-    @ManyToOne(cascade = CascadeType.ALL)
+
+    @ManyToOne
     @JoinColumn(name="customerId")
     private Customer customer;
 
